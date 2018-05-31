4ba3bd26369cd9be4cde5c8715d33fc40d8f194b
Olga <oserenchenko@gmail.com>
2018-05-30 20:39:07 -0600

    random selection of word and display of blanks for each character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,26 @@
 //Array of countries - the computer will randomly select one of these and the player will have to guess it
 wordsToGuess = ["Argentina", "Bulgaria", "Denmark", "Ghana", "Honduras", "Israel", "Japan", "Mongolia", "Myanmar", "Philippines", "Spain", "Sweden", "Uganda", "Vietnam", "Yemen", ];
 
-// Computer randomly chooses a word from the wordsToGuess array - this becomes the chosenWord
-var chosenWord = document.querySelector("#word");
-console.log(chosenWord.innerHTML);
+// Function that lets the computer randomly choose a word from the wordsToGuess array - the chose word becomes "randomSelect"
+function chooseNewWord() {
+  var randomSelect = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+  return randomSelect;
+}
+//runs the above function
+var randomWord = chooseNewWord();
+console.log(randomWord); //logging it to the console just for now, for troubleshooting
 
-
+//Function that that the "randomWord" chosen above and replaces each string with a "___" and then displays it in the html header tag with the "chosenWordBlanks" ID
+function hideWord(randomWord) {
+  var blanks = ""
+  for (i = 0; i < randomWord.length; i++) {
+    blanks += "____    ";
+  }
+  var chosenWord = document.querySelector("#chosenWordBlanks");
+  chosenWord.innerHTML = blanks;
+}
+//runs the above function 
+hideWord(randomWord);
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function () {
